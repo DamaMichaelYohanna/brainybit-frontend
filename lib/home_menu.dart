3cc683d54a6f1e25e2c643c4db67df743c42d175
= <get2dama11@gmail.com>
2024-02-12 11:43:35 +0100

    changed name of file

---
+++ lib/home_menu.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:konnet/index.dart';
+import 'package:konnet/quiz.dart';
+import 'colorScheme.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void _tabSwitch(int index) {
+    setState(() {
+      _selectedIndex = index;
+      debugPrint("clicked");
+      print(index);
+    });
+  }
+
+  List<Widget> tabScreen = [
+    const IndexPage(),
+    const QuizScreen(),
+    const Text("Third Screen!")
+  ];
+
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("BrainyBit"),
+          centerTitle: true,
+          leading: const Icon(Icons.computer),
+          actions: const [Icon(Icons.notifications)]),
+      body: tabScreen.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.chat_sharp), label: "ChatAI"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.admin_panel_settings), label: "Profile")
+        ],
+        onTap: _tabSwitch,
+        currentIndex: _selectedIndex,
+      ),
+    );
+  }
+}
