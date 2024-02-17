cd76c0a9ea3e1cbbd929d3d3a15e085709b977e6
= <get2dama11@gmail.com>
2024-02-17 18:36:31 +0100

    updated startup file

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:konnet/colorScheme.dart';
 import 'package:konnet/home_menu.dart';
-// import 'package:konnet/login.dart';
-import 'package:konnet/quiz.dart';
+import 'package:konnet/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+Future<void> main() async {
+  // Function to fetch user token to check if the user is logged in.
+  WidgetsFlutterBinding.ensureInitialized();
+  Future<String> getUserToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString("token") ?? "";
+  }
+
+  var token = await getUserToken();
   runApp(MaterialApp(
-      title: "Nsuk-Online",
+      title: "BrainyBit",
       theme: ThemeData(
         primarySwatch: mine,
         //<-- SEE HERE
       ),
-      home: const HomePage()));
+      home: token.isNotEmpty ? const HomePage() : const LoginScreen()));
 }
+
+// Future<bool> setUserName(String value) async {
+//   final SharedPreferences prefs = await SharedPreferences.getInstance();
+//   return prefs.setString("username", value);
+// }
+
+// Future<String> getUserName(String value) async {
+//   final SharedPreferences prefs = await SharedPreferences.getInstance();
+//   return prefs.getString("username");
+// }
