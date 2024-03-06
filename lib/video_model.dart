138e5ce921d2d6b24e4fe2c5d25bca29d1e903f7
= <get2dama11@gmail.com>
2024-03-06 22:27:43 +0100

    updated video list model

---
+++ lib/video_model.dart
@@ -0,0 +1,13 @@
+Map<String, Map> videoList = {
+  "GST": {"-Q1p2zEbT3c": "kNCVFtmhR8I"},
+  "CMP": {
+    "CMP 211": [
+      "rRXDC4DX11g",
+      "rJhYsSD-K-s",
+      "3QxrtQ-yFOA",
+      "-B3KEcL52lc",
+      "-Q1p2zEbT3c"
+    ],
+    "CMP 224": ["Cd9sCMzEKEk", "kNCVFtmhR8I", "tLdS3YCP7CI"]
+  }
+};
