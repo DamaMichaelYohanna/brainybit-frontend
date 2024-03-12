bea98180b1da858a057d8638365f169e98689900
= <get2dama11@gmail.com>
2024-03-12 19:25:47 +0100

    updated the video model

--- lib/video_model.dart
+++ lib/video_model.dart
@@ -1,13 +1,20 @@
-Map<String, Map> videoList = {
-  "GST": {"-Q1p2zEbT3c": "kNCVFtmhR8I"},
+Map<String, Map<String, Map<String, String>>> videoList = {
+  // "GST": {"-Q1p2zEbT3c": "kNCVFtmhR8I"},
   "CMP": {
-    "CMP 211": [
-      "rRXDC4DX11g",
-      "rJhYsSD-K-s",
-      "3QxrtQ-yFOA",
-      "-B3KEcL52lc",
-      "-Q1p2zEbT3c"
-    ],
-    "CMP 224": ["Cd9sCMzEKEk", "kNCVFtmhR8I", "tLdS3YCP7CI"]
+    "CMP 211": {
+      "rRXDC4DX11g": "Introduction to Programming",
+      "rJhYsSD-K-s": "Java for beginner",
+      "3QxrtQ-yFOA": "Datatypes In Java Programming.",
+      "-B3KEcL52lc": "Variable In Java Programming",
+      "-Q1p2zEbT3c": "Arithemtic Operator in Java"
+    },
+    "CMP 224": {
+      "rRXDC4DX11g": "Introduction to Web development",
+      "rJhYsSD-K-s": "What is the Internet all about",
+      "3QxrtQ-yFOA": "HTML at a glance",
+      "-B3KEcL52lc": "Variable In Java Programming",
+      "-Q1p2zEbT3c": "Arithemtic Operator in Java"
+    },
+    // "CMP 224": ["Cd9sCMzEKEk", "kNCVFtmhR8I", "tLdS3YCP7CI"]
   }
 };
