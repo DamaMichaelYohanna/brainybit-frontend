a3551cf1e712895e1d394ead7a474192071676c7
= <get2dama11@gmail.com>
2024-03-12 19:33:18 +0100

    added file for course details

---
+++ lib/course_detail.dart
@@ -0,0 +1,6 @@
+Map<String, String> courseDetail = {
+  "CMP 211":
+      "This is an introductory course into programming using Java Programming lanaguage",
+  "CMP 224": "INFO COMING SOON",
+  "CMP 213": "This course is about electronic circuitry."
+};
