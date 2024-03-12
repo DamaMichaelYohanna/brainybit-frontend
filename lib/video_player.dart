6947854f1cc9aa8efef441c221ee15129fd8bbfd
= <get2dama11@gmail.com>
2024-03-12 19:25:26 +0100

    updated video playter

--- lib/video_player.dart
+++ lib/video_player.dart
@@ -19,17 +19,18 @@ class _YoutubePlayerScreenState extends State<YoutubePlayerScreen> {
     late final videoId = widget.videoId;
     // TO load a video by its unique id
     _controller = YoutubePlayerController(
-      initialVideoId: videoId,
-    );
+        initialVideoId: videoId,
+        flags: const YoutubePlayerFlags(
+          autoPlay: false,
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          // Youtube player as widget
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
           child: YoutubePlayer(
             controller: _controller, // Controler that we created earlier
             aspectRatio: 16 / 9, // Aspect ratio you want to take in screen
