204eb533e4ed78d2f48e86b8dd3f974f563eb46b
= <get2dama11@gmail.com>
2024-03-23 09:17:31 +0100

    update course listing page

--- lib/course_list.dart
+++ lib/course_list.dart
@@ -37,98 +37,69 @@ Map<String, Map<String, Map<String, String>>> department = {
     }
   },
 };
-
-// class EachListTile extends StatelessWidget {
-//   final String dept;
-//   EachListTile({super.key, required this.dept});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: department[dept]?.length,
-//       itemBuilder: (BuildContext context, int index) {
-//         String courseCode = department[dept]!.keys.elementAt(index);
-//         String courseName = department[dept]!.values.elementAt(index);
-//         print(courseCode);
-//         Random random = Random();
-
-//         return Card(
-//           color: const Color.fromARGB(255, 255, 255, 255),
-//           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
-//           child: ListTile(
-//             leading: Container(
-//               // height: 180,
-//               // width: 60,
-//               color: Colors.amber,
-//               // child: images[random.nextInt(images.length)],
+// Row(
+//   children: [
+//     InkWell(
+//       child: const Text("Resources |",
+//           style: TextStyle(
+//               fontWeight: FontWeight.bold)),
+//       onTap: () {
+//         debugPrint(dept);
+//         if (videoList[dept]!
+//             .containsKey(entry.key)) {
+//           Navigator.of(context).push(
+//             MaterialPageRoute(
+//               builder: (context) =>
+//                   VideoPlayList(
+//                 courseCode: entry.key,
+//                 courseName: entry.value,
+//               ),
 //             ),
-//             title: Text(courseCode),
-//             subtitle:
-//                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-//               Text(courseName),
-//               const Divider(),
-//               Row(
-//                 children: [
-//                   InkWell(
-//                       child: const Text("Resources |",
-//                           style: TextStyle(fontWeight: FontWeight.bold)),
-//                       onTap: () {
-//                         debugPrint(dept);
-//                         if (videoList[dept]!.containsKey(
-//                             department[dept]!.keys.elementAt(index))) {
-//                           Navigator.of(context).push(
-//                             MaterialPageRoute(
-//                               builder: (context) => VideoPlayList(
-//                                 courseCode: courseCode,
-//                                 courseName: courseName,
-//                               ),
-//                             ),
-//                           );
-//                         } else {
-//                           showDialog(
-//                               context: context,
-//                               builder: (_) => const AlertDialog(
-//                                     title: Text('Notice!'),
-//                                     // icon:Text("hell"),
-//                                     content:
-//                                         Text("No Resource For this course yet"),
-//                                   ));
-//                         }
-//                       }),
-//                   InkWell(
-//                     onTap: () {
-//                       if (questions.containsKey(
-//                           department[dept]!.keys.elementAt(index))) {
-//                         Navigator.of(context).push(
-//                           MaterialPageRoute(
-//                             builder: (context) => QuizScreen(
-//                               courseName:
-//                                   department[dept]!.keys.elementAt(index),
-//                             ),
-//                           ),
-//                         );
-//                       } else {
-//                         showDialog(
-//                             context: context,
-//                             builder: (_) => const AlertDialog(
-//                                   title: Text('Notice!'),
-//                                   // icon:Text("hell"),
-//                                   content: Text("No quiz added yet"),
-//                                 ));
-//                       }
-//                     },
-//                     child: const Text("  Quiz",
-//                         style: TextStyle(fontWeight: FontWeight.bold)),
-//                   ),
-//                 ],
-//               )
-//             ]),
-//           ),
-//         );
+//           );
+//         } else {
+//           showDialog(
+//               context: context,
+//               builder: (_) =>
+//                   const AlertDialog(
+//                     title: Text('Notice!'),
+//                     // icon:Text("hell"),
+//                     content: Text(
+//                         "No Resource For this course yet oh"),
+//                   ));
+//         }
 //       },
-//     );
-//   }
-// }
+//     ),
+//     InkWell(
+//       onTap: () {
+//         print(entry.value);
+//         if (questions
+//             .containsKey(entry.key)) {
+//           Navigator.of(context).push(
+//             MaterialPageRoute(
+//               builder: (context) =>
+//                   QuizScreen(
+//                 courseName: entry.key,
+//               ),
+//             ),
+//           );
+//         } else {
+//           showDialog(
+//               context: context,
+//               builder: (_) =>
+//                   const AlertDialog(
+//                     title: Text('Notice!'),
+//                     // icon:Text("hell"),
+//                     content: Text(
+//                         "No quiz added yet"),
+//                   ));
+//         }
+//       },
+//       child: const Text("  Quiz",
+//           style: TextStyle(
+//               fontWeight: FontWeight.bold)),
+//     ),
+//   ],
+// ),
 
 class CourseListTile extends StatefulWidget {
   final String dept;
@@ -203,89 +174,32 @@ class _CourseListTileState extends State<CourseListTile>
                 children: department[dept]!.keys.map((String key) {
                   return ListView(
                     children: department[dept]![key]!.entries.map((entry) {
-                      return Card(
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(0)),
-                          child: ListTile(
-                            leading: Container(
-                              height: 60,
-                              width: 60,
-                              padding: EdgeInsets.all(3),
-                              color: Color.fromARGB(255, 247, 242, 228),
-                              child: Icon(Icons.book),
+                      return InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => VideoPlayList(
+                                courseCode: entry.key,
+                                courseName: entry.value,
+                              ),
                             ),
-                            title: Text(entry.key),
-                            subtitle: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(entry.value),
-                                  const Divider(),
-                                  Row(
-                                    children: [
-                                      InkWell(
-                                        child: const Text("Resources |",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold)),
-                                        onTap: () {
-                                          debugPrint(dept);
-                                          if (videoList[dept]!
-                                              .containsKey(entry.key)) {
-                                            Navigator.of(context).push(
-                                              MaterialPageRoute(
-                                                builder: (context) =>
-                                                    VideoPlayList(
-                                                  courseCode: entry.key,
-                                                  courseName: entry.value,
-                                                ),
-                                              ),
-                                            );
-                                          } else {
-                                            showDialog(
-                                                context: context,
-                                                builder: (_) =>
-                                                    const AlertDialog(
-                                                      title: Text('Notice!'),
-                                                      // icon:Text("hell"),
-                                                      content: Text(
-                                                          "No Resource For this course yet oh"),
-                                                    ));
-                                          }
-                                        },
-                                      ),
-                                      InkWell(
-                                        onTap: () {
-                                          print(entry.value);
-                                          if (questions
-                                              .containsKey(entry.key)) {
-                                            Navigator.of(context).push(
-                                              MaterialPageRoute(
-                                                builder: (context) =>
-                                                    QuizScreen(
-                                                  courseName: entry.key,
-                                                ),
-                                              ),
-                                            );
-                                          } else {
-                                            showDialog(
-                                                context: context,
-                                                builder: (_) =>
-                                                    const AlertDialog(
-                                                      title: Text('Notice!'),
-                                                      // icon:Text("hell"),
-                                                      content: Text(
-                                                          "No quiz added yet"),
-                                                    ));
-                                          }
-                                        },
-                                        child: const Text("  Quiz",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold)),
-                                      ),
-                                    ],
-                                  ),
-                                ]),
-                          ));
+                          );
+                        },
+                        child: Card(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(0)),
+                            child: ListTile(
+                                leading: Container(
+                                  height: 60,
+                                  width: 60,
+                                  padding: EdgeInsets.all(3),
+                                  color: Color.fromARGB(255, 247, 242, 228),
+                                  child: Icon(Icons.book),
+                                ),
+                                title: Text(entry.key),
+                                subtitle: Text(entry.value))),
+                      );
                     }).toList(),
                   );
                 }).toList(),
