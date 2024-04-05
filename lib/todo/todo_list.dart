2c43da88c0e261c6b597f1999d932b1683c6f8a8
= <get2dama11@gmail.com>
2024-04-05 06:57:54 +0100

    rewrote the view

--- lib/todo/todo_list.dart
+++ lib/todo/todo_list.dart
@@ -11,7 +11,7 @@ Map<String, List> tasks = {
     "Come let's go for and outing in my fathers farm and check out some stuff there for recreation and study"
   ],
   "today": ["Go to school", "Goto shop", "Finally go to church"],
-  "Tomorrow": ["orientation", "Code a little", "All good"]
+  "Tomorrow": ["orientation", "Code a little", "All good"],
 };
 
 class TodoListPage extends StatefulWidget {
@@ -29,6 +29,22 @@ class _TodoListPageState extends State<TodoListPage>
   bool? isSelected = false;
   Map<String, bool> checkboxStates =
       {}; // Map to hold checkbox state for each task
+  List<Map<String, dynamic>> taskList = [];
+
+  void fetch() async {
+    // Await the result of the asynchronous database query
+
+    await databaseHelper.queryAllRows().then((value) => {
+          taskList = value,
+          print(value),
+          if (mounted)
+            {
+              setState(() {
+                fetch();
+              })
+            }
+        });
+  }
 
   @override
   void initState() {
@@ -61,85 +77,39 @@ class _TodoListPageState extends State<TodoListPage>
               ))
         ],
       ),
-      body: Column(
-        children: [
-          // const Padding(
-          //   padding: EdgeInsets.all(8.0),
-          //   child: Text(
-          //     "Tasks",
-          //     textAlign: TextAlign.end,
-          //     style: TextStyle(fontSize: 22),
-          //   ),
-          // ),
-          // const Divider(),
-          Container(
-            height: 55,
-            padding: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              // color: Colors.white,
-              borderRadius: BorderRadius.circular(
-                2.0,
-              ),
-            ),
-            child: TabBar(
-              controller: _tabController,
-              // give the indicator a decoration (border)
-              indicator: const BoxDecoration(
-                border: Border(
-                    bottom: BorderSide(
-                  color: Colors.amber,
-                  width: 2.0,
-                )),
-              ),
-              labelColor: Colors.brown,
-              unselectedLabelColor: Colors.black,
-
-              tabs: tasks.keys.map((title) => Tab(text: title)).toList(),
-            ),
-          ),
-          //
-          Expanded(
-            child: TabBarView(
-              controller: _tabController,
-              children: tasks.keys.map((mapKey) {
-                return ListView.builder(
-                  itemCount: tasks[mapKey]!.length,
-                  itemBuilder: (context, index) {
-                    String saga = tasks[mapKey]![index];
-                    return ListTile(
-                      title: Container(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 240, 192, 49),
-                            borderRadius: BorderRadius.circular(5)),
-                        padding: EdgeInsets.only(top: 10, left: 12, bottom: 10),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  tasks[mapKey]![index],
-                                  // overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                              Checkbox(
-                                  activeColor: Colors.white,
-                                  checkColor: Colors.brown,
-                                  value: checkboxStates[saga] ?? false,
-                                  onChanged: (value) {
-                                    print(saga);
-                                    setState(() {
-                                      checkboxStates[saga] = value ?? false;
-                                    });
-                                  })
-                            ]),
+      body: ListView.builder(
+        itemCount: taskList.length,
+        itemBuilder: (context, index) {
+          Map<String, dynamic> saga = taskList[index];
+          return ListTile(
+            title: Container(
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 240, 192, 49),
+                  borderRadius: BorderRadius.circular(5)),
+              padding: EdgeInsets.only(top: 10, left: 12, bottom: 10),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "${saga["title"]}",
+                        // overflow: TextOverflow.ellipsis,
                       ),
-                    );
-                  },
-                );
-              }).toList(),
+                    ),
+                    Checkbox(
+                        activeColor: Colors.white,
+                        checkColor: Colors.brown,
+                        value: checkboxStates[saga] ?? false,
+                        onChanged: (value) {
+                          print(saga);
+                          setState(() {
+                            checkboxStates[saga["title"]] = value ?? false;
+                          });
+                        })
+                  ]),
             ),
-          ),
-        ],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
