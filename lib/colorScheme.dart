0714cb427a6df199f3759d3a53f4e97dadd1faa0
= <get2dama11@gmail.com>
2024-04-09 15:27:13 +0100

    changed color scheme

--- lib/colorScheme.dart
+++ lib/colorScheme.dart
@@ -2,24 +2,23 @@
 import 'package:flutter/material.dart';
 
 const MaterialColor mine = MaterialColor(_minePrimaryValue, <int, Color>{
-  50: Color(0xFFFDF8E6),
-  100: Color(0xFFFBEDC2),
-  200: Color(0xFFF8E199),
-  300: Color(0xFFF5D470),
-  400: Color(0xFFF3CB51),
+  50: Color(0xFFEAEEF2),
+  100: Color(0xFFCAD5DF),
+  200: Color(0xFFA7B9CA),
+  300: Color(0xFF839DB4),
+  400: Color(0xFF6988A4),
   500: Color(_minePrimaryValue),
-  600: Color(0xFFEFBC2D),
-  700: Color(0xFFEDB426),
-  800: Color(0xFFEBAC1F),
-  900: Color(0xFFE79F13),
+  600: Color(0xFF476B8C),
+  700: Color(0xFF3D6081),
+  800: Color(0xFF355677),
+  900: Color(0xFF254365),
 });
-
-const int _minePrimaryValue = 0xFFF1C232;
+const int _minePrimaryValue = 0xFF4E7394;
 
 const MaterialColor mineAccent = MaterialColor(_mineAccentValue, <int, Color>{
-  100: Color(0xFFFFFFFF),
+  100: Color(0xFFA8CFFF),
   200: Color(_mineAccentValue),
-  400: Color(0xFFFFE2AF),
-  700: Color(0xFFFFD896),
+  400: Color(0xFF4298FF),
+  700: Color(0xFF298AFF),
 });
-const int _mineAccentValue = 0xFFFFF4E2;
+const int _mineAccentValue = 0xFF75B3FF;
