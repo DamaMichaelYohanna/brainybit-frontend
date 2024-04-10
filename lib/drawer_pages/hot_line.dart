7c4844e906f7399f707b3a4e43f6d8ea9eb6ea06
DamaMichaelYohanna <get2dama11@gmail.com>
2024-04-10 23:39:30 +0100

    updated hotlines page

---
+++ lib/drawer_pages/hot_line.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:konnet/colorScheme.dart';
+
+class HotLine extends StatelessWidget {
+  const HotLine({super.key});
+
+  Future<void> _launchUrl(_url) async {
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: mine,
+        foregroundColor: Colors.white,
+        title: const Text("HotLines"),
+      ),
+      body: ListView(
+        children: const [
+          ContactList(
+              contact: "08063314090",
+              contactName: "School Clinic",
+              iconName: Icons.local_hospital),
+          Divider(
+            height: 1,
+            color: Color.fromARGB(255, 249, 247, 247),
+          ),
+          ContactList(
+            contact: "07063109604",
+            contactName: "Security Unit",
+            iconName: Icons.security,
+          ),
+          Divider(
+            height: 1,
+            color: Color.fromARGB(255, 249, 247, 247),
+          ),
+          ContactList(
+            contact: "08080237035",
+            contactName: "Fire Service",
+            iconName: Icons.fire_truck,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ContactList extends StatelessWidget {
+  final String contact;
+  final String contactName;
+  final IconData iconName;
+  const ContactList(
+      {super.key,
+      required this.contact,
+      required this.contactName,
+      required this.iconName});
+
+  Future<void> _launchUrl(_url) async {
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(contactName),
+      subtitle: Text(contact),
+      trailing: IconButton(
+          onPressed: () {
+            Clipboard.setData(ClipboardData(text: contact));
+            final snackBar = SnackBar(
+              content: const Text('Copied to clibboard'),
+              // backgroundColor: returnValue ? Colors.green : Colors.red,
+              action: SnackBarAction(
+                label: '',
+                onPressed: () {},
+              ),
+            );
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+          icon: const Icon(
+            Icons.copy_all,
+            color: mine,
+          )),
+      leading: CircleAvatar(child: Icon(iconName)),
+      onTap: () {
+        _launchUrl(Uri.parse('tel:$contact'));
+      },
+    );
+  }
+}
