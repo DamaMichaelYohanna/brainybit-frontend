1a71761ae850b4fcf0b6142798ea177079f2d701
DamaMichaelYohanna <get2dama11@gmail.com>
2024-04-10 17:28:58 +0100

    added drawer

--- lib/home_menu.dart
+++ lib/home_menu.dart
@@ -28,24 +28,97 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: const Text("BrainyBit"),
-          backgroundColor: mine,
-          centerTitle: true,
-          leading: const Icon(Icons.menu),
-          actions: const [Icon(Icons.notifications)]),
-      body: tabScreen.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.chat_sharp), label: "ChatAI"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.admin_panel_settings), label: "Profile")
-        ],
-        onTap: _tabSwitch,
-        currentIndex: _selectedIndex,
-      ),
-    );
+        appBar: AppBar(
+            title: const Text("BrainyBit"),
+            backgroundColor: mine,
+            centerTitle: true,
+            // leading: const Icon(Icons.menu),
+            actions: const [Icon(Icons.notifications)]),
+        body: tabScreen.elementAt(_selectedIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.chat_sharp), label: "ChatAI"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.admin_panel_settings), label: "Profile")
+          ],
+          onTap: _tabSwitch,
+          currentIndex: _selectedIndex,
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.white,
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Image.asset("assets/images/logo.png"),
+              ),
+              ListTile(
+                title: const Text("NSUK Hot Lines"),
+                leading: const Icon(Icons.call),
+                onTap: (() {}),
+              ),
+              const Divider(
+                color: Colors.white,
+              ),
+              ListTile(
+                title: const Text("Hire A tutor"),
+                leading: const Icon(Icons.work),
+                onTap: () {},
+              ),
+              const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  "Special",
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
+              const Divider(
+                color: Colors.white,
+              ),
+              ListTile(
+                title: const Text("Make Donation"),
+                leading: const Icon(Icons.attach_money),
+                onTap: () {},
+              ),
+              const Divider(
+                color: Colors.white,
+              ),
+              ListTile(
+                  title: const Text("About BrainyBit"),
+                  leading: const Icon(Icons.apps_outage),
+                  // tileColor: Colors.white,
+                  onTap: (() {})),
+              const Divider(
+                color: Colors.white,
+              ),
+              ListTile(
+                  title: const Text("Be A Contributor"),
+                  leading: const Icon(Icons.bubble_chart),
+                  // tileColor: Colors.white,
+                  onTap: (() {})),
+              const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  "More",
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
+              const Divider(
+                color: Colors.white,
+              ),
+              ListTile(
+                title: const Text("Drop Suggestion"),
+                leading: const Icon(Icons.local_offer_rounded),
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text("Share application"),
+                leading: const Icon(Icons.share),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ));
   }
 }
