831bd50113db45a14225ec9ddcbf86d75a720b6f
DamaMichaelYohanna <get2dama11@gmail.com>
2024-04-14 23:40:06 +0100

    updated the calender page

--- lib/calender.dart
+++ lib/calender.dart
@@ -13,11 +13,44 @@ class _CalenderState extends State {
   final GroupedItemScrollController itemScrollController =
       GroupedItemScrollController();
   final List<Map<String, dynamic>> _elements = [
-    {'group': 'March 2024', 'name': 'Element 1'},
-    {'group': 'March 2024', 'name': 'Element 2'},
-    {'group': 'April 2024', 'name': 'Element 3'},
-    {'group': 'Group B', 'name': 'Element 4'},
-    {'group': 'Group B', 'name': 'Element 5'},
+    {'group': 'MAY, 2024', 'name': 'Public Holiday', 'date': 'Wednesday 1st'},
+    {
+      'group': 'MAY, 2024',
+      'name': 'Lecture closes For First semester',
+      'date': 'Saturday, 4th'
+    },
+    {'group': 'MAY, 2024', 'name': 'Revision', 'date': 'Monday 6th'},
+    {
+      'group': 'MAY, 2024',
+      'name': 'Exams (Faculty of education)',
+      'date': 'Monday 13th'
+    },
+    {
+      'group': 'MAY, 2024',
+      'name': 'Exams(Other faculty)',
+      'date': 'Monday 20th'
+    },
+    {
+      'group': 'JUNE, 2024',
+      'name': 'Marking and endorsement',
+      'date': '10 June 2024'
+    },
+    {
+      'group': 'JUNE, 2024',
+      'name': 'Commencement of siwes',
+      'date': '10 June 2024'
+    },
+    {'group': 'JUNE, 2024', 'name': 'Public Holiday', 'date': '12 June 2024'},
+    {
+      'group': 'JUNE, 2024',
+      'name': 'Resumption of second semester',
+      'date': ' 23 June 2024'
+    },
+    {
+      'group': 'JUNE, 2024',
+      'name': 'Regular Senate meeting',
+      'date': ' 27 June 2024'
+    },
   ];
 
   @override
@@ -43,9 +76,10 @@ class _CalenderState extends State {
 
           itemBuilder: (context, dynamic element) => EventTile(
             eventTitle: '${element['name']}',
+            eventDate: '${element['date']}',
           ),
-          itemComparator: (e1, e2) =>
-              e1['name'].compareTo(e2['name']), // optional
+          // itemComparator: (e1, e2) =>
+          //     e1['date'].compareTo(e2['date']), // optional
           elementIdentifier: (element) =>
               element.name, // optional - see below for usage
           // itemScrollController: itemScrollController, // optional
@@ -58,18 +92,20 @@ class _CalenderState extends State {
 
 class EventTile extends StatelessWidget {
   final String eventTitle;
-  const EventTile({super.key, required this.eventTitle});
+  final String eventDate;
+  const EventTile(
+      {super.key, required this.eventTitle, required this.eventDate});
 
   @override
   Widget build(BuildContext context) {
     return Card(
         elevation: 0,
-        color: Color.fromARGB(255, 228, 235, 241),
+        color: const Color.fromARGB(255, 228, 235, 241),
         margin: const EdgeInsets.all(10),
         child: ListTile(
           title: Text(eventTitle),
-          subtitle: Text("12/12/2024"),
-          leading: Icon(Icons.calendar_month),
+          subtitle: Text(eventDate),
+          leading: const Icon(Icons.calendar_month),
         ));
   }
 }
