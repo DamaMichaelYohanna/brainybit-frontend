a31cd9d3e5c3c92cd0aef027d7b519de9b2daa7f
DamaMichaelYohanna <get2dama11@gmail.com>
2024-04-16 23:58:59 +0100

    added collab page

---
+++ lib/drawer_pages/collab.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:konnet/colorScheme.dart';
+
+class Collab extends StatefulWidget {
+  const Collab({super.key});
+
+  @override
+  State<StatefulWidget> createState() => CollabState();
+}
+
+class CollabState extends State<Collab> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mine,
+        foregroundColor: mine,
+      ),
+      body: ListView(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 170,
+            color: mine,
+            padding: EdgeInsets.all(12),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "Are you a developer?",
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Text(
+                    "This project is open source and thus open for collaboration. If you are a developer with relevant skill in python(fastAPI) and dart(flutter), also wish to contribute to this project reach out to us",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 10.0, top: 10),
+            child: Text("Name"),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0, bottom: 10, right: 10),
+            child: TextField(),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 10.0, bottom: 10, right: 10),
+            child: Text("Phone"),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0, bottom: 10, right: 10),
+            child: TextField(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0, bottom: 10, right: 10),
+            child: Text("Email"),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0, bottom: 10, right: 10),
+            child: TextField(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0, bottom: 10, right: 10),
+            child: TextField(
+              maxLines: 5,
+              decoration: InputDecoration(
+                hintText: "Any other thing to let us know?",
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: mine,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5))),
+              child: const Text("Send Request"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
