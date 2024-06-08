5dfeb951d5eaa6db935530b54894926f7949127d
DamaMichaelYohanna <get2dama11@gmail.com>
2024-06-08 08:59:39 +0100

    added quiz finish page

---
+++ lib/quiz_finish.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class QuizFinish extends StatelessWidget {
+  const QuizFinish({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: []
+      )
+    );
+  }
+}
