2b94cd4b551a1f8bb5b83eeba0dee2cee6c2372e
DamaMichaelYohanna <get2dama11@gmail.com>
2024-06-11 10:48:16 +0100

    updated the about page with container

--- lib/about.dart
+++ lib/about.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:konnet/colorScheme.dart';
-import 'package:konnet/main.dart';
-import 'package:konnet/subscribe.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutUsPage extends StatelessWidget {
   const AboutUsPage({super.key});
@@ -28,71 +28,133 @@ class AboutUsPage extends StatelessWidget {
             ),
           ),
           const Padding(
-            padding: EdgeInsets.only(bottom: 8.0),
-            child: Text("Special Welcome Once More ",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 15, color: mine)),
+            padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 5),
+            child: Text(
+                "BrainyBit is a one stop for all app tailored toward NSUK Students to provide them with past questions, learning resources, interface for note taking and todos among others ",
+                // textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 16, color: Color.fromARGB(255, 33, 68, 107))),
           ),
-          Container(
-            // height: 200,
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                image: DecorationImage(
-                    image: AssetImage("assets/images/ture.png"),
-                    fit: BoxFit.cover)),
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "Your Score",
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(" Points",
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                ),
-                // display message base on user status
-                !isPremium
-                    ? Column(
-                        children: [
-                          const Divider(),
-                          const Text(
-                              "You are on free plan with only 10 free questions. Get more with premium subscription",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: Colors.red)),
-                          ElevatedButton(
-                              onPressed: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => const SubscribePage(),
-                                ));
-                              },
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: mine,
-                                  foregroundColor: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(5))),
-                              child: const Text("Subscribe To Premuim")),
-                        ],
-                      )
-                    : ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: mine,
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5))),
-                        child: const Text("Close")),
-              ],
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5),
+            child: Text(
+              "Vision",
+              style: TextStyle(
+                  fontSize: 15, fontWeight: FontWeight.bold, color: mine),
             ),
           ),
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.symmetric(horizontal: 18.0, vertical: 5),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 236, 241, 245),
+                borderRadius: BorderRadius.circular(10)),
+            child: Text(
+                "My vission is to empower every student with accessible and comprehensive educational resources, fostering a dynamic learning environment that transcends traditional boundaries.",
+                style: TextStyle(
+                    fontSize: 16, color: Color.fromARGB(255, 2, 8, 14))),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5),
+            child: Text(
+              "Mission",
+              style: TextStyle(
+                  fontSize: 15, fontWeight: FontWeight.bold, color: mine),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.symmetric(horizontal: 18.0, vertical: 5),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 236, 241, 245),
+                borderRadius: BorderRadius.circular(10)),
+            child: Text(
+                "My mission is to provide a centralized platform for students, offering past questions, learning materials, and essential school information, thereby enhancing academic excellence, promoting student success, and cultivating a vibrant community of lifelong learners"),
+          ),
+          // const Divider(),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5),
+            child: Text(
+              "Meet The Developer",
+              style: TextStyle(
+                  fontSize: 15, fontWeight: FontWeight.bold, color: mine),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 5),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 236, 241, 245),
+                borderRadius: BorderRadius.circular(10)),
+            child: const Text(
+                "I'm Dama Michael Yohanna, a student from the prestigous department of Computer Science, Nasarawa State University, who derives joy in problem solving. On my free, I love to study and listen to music"),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
+            child: Text(
+              "Let's Connect",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 15, fontWeight: FontWeight.bold, color: mine),
+            ),
+          ),
+          // const Padding(
+          //   padding: EdgeInsets.all(10.0),
+          //   child: Text("You can reach me via the following social space"),
+          // ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(FontAwesomeIcons.facebook)),
+                IconButton(
+                    onPressed: () async {
+                      String contact = "+2348160535033";
+                      String text = 'Hello';
+                      String androidUrl =
+                          "whatsapp://send?phone=$contact&text=$text";
+                      await launchUrl(Uri.parse(androidUrl));
+                    },
+                    icon: const Icon(FontAwesomeIcons.whatsapp)),
+                IconButton(
+                    onPressed: () async {
+                      String androidUrl =
+                          "https://www.linkedin.com/in/dama-michael-yohanna-2b9835230/";
+                      await launchUrl(Uri.parse(androidUrl));
+                    },
+                    icon: const Icon(FontAwesomeIcons.linkedin)),
+                IconButton(
+                    onPressed: () async {
+                      String androidUrl =
+                          "https://www.tiktok.com/@codewithdama";
+                      await launchUrl(Uri.parse(androidUrl));
+                    },
+                    icon: const Icon(FontAwesomeIcons.tiktok)),
+                IconButton(
+                    onPressed: () async {
+                      String androidUrl = "https://x.com/dama_michael";
+                      await launchUrl(Uri.parse(androidUrl));
+                    },
+                    icon: Icon(FontAwesomeIcons.twitter)),
+                IconButton(
+                    onPressed: () async {
+                      String androidUrl = "mailto:get2dama11@gmail.com";
+                      await launchUrl(Uri.parse(androidUrl));
+                    },
+                    icon: Icon(Icons.mail)),
+                IconButton(
+                    onPressed: () async {
+                      String androidUrl =
+                          "https://github.com/DamaMichaelYohanna";
+                      await launchUrl(Uri.parse(androidUrl));
+                    },
+                    icon: Icon(FontAwesomeIcons.github)),
+              ],
+            ),
+          )
         ]));
   }
 }
