380c842a750b8acc22cf6ddbb15f2e6ab0488d6c
DamaMichaelYohanna <get2dama11@gmail.com>
2024-10-21 23:34:34 +0100

    updated quiz page to fetch from online

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,21 +1,23 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:brainybit/colorScheme.dart';
 import 'package:brainybit/main.dart';
-import 'package:brainybit/questions_model.dart';
+// import 'package:brainybit/questions_model.dart';
+import 'package:http/http.dart' as http;
 import 'package:brainybit/quiz_finish.dart';
 
 class QuizScreen extends StatefulWidget {
-  final String courseName;
-  const QuizScreen({key, required this.courseName}) : super(key: key);
+  final String courseCode;
+  const QuizScreen({super.key, required this.courseCode});
 
   @override
   // ignore: no_logic_in_create_state
-  State<QuizScreen> createState() => _QuizScreenState(courseName: courseName);
+  State<QuizScreen> createState() => _QuizScreenState();
 }
 
 class _QuizScreenState extends State<QuizScreen> {
-  late String courseName;
-  _QuizScreenState({required this.courseName});
+  _QuizScreenState();
 
   void reset() {
     setState(() {
@@ -25,10 +27,43 @@ class _QuizScreenState extends State<QuizScreen> {
     });
   }
 
-// filter/narrow down te question map
+  Future<String> getData() {
+    return Future.delayed(const Duration(seconds: 0), () async {
+      if (questions.containsKey(widget.courseCode)) {
+        // no need to run the function since questions already exist.
+        return "";
+      }
+      print("Damn you");
+      // if question are not loaded, try fetch them online.
+      var url = Uri.https('brainybit.vercel.app',
+          'api/v1/general/question/${widget.courseCode}');
+      try {
+        final response = await http.get(url);
+        print(response.statusCode);
+        print(response.body);
+        print(questions);
+        if (response.statusCode == 200) {
+          questions[widget.courseCode] = json.decode(response.body);
+          print(questions);
+          print("not working");
+          return "done";
+        } else {
+          throw Exception(
+              'Failed to load video details'); // Handle non-200 status codes
+        }
+      } catch (e) {
+        // Log or handle the error appropriately
+        rethrow; // Re-throw for FutureBuilder to handle
+      }
+      // throw Exception("Custom Error");
+    });
+  }
+
+// filter/narrow down the question map
   // check if user is premium or not to decide number of questions
-  late List<Map<String, dynamic>> filteredQuestions =
-      isPremium ? questions[courseName]! : questions[courseName]!.sublist(0, 5);
+  late List<dynamic> filteredQuestions = isPremium
+      ? questions[widget.courseCode]!
+      : questions[widget.courseCode]!.sublist(0, 30);
 // Call back function for grading.
   bool grade(selectedOption, correctOption) {
     int position =
@@ -96,94 +131,112 @@ class _QuizScreenState extends State<QuizScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("$courseName Quiz"),
-      ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                // Expanded(
-                //   child: Slider(
-                //       activeColor: mine,
-                //       thumbColor: mine.shade100,
-                //       value: _currentSliderValue,
-                //       max: filteredQuestions.length.toDouble(),
-                //       // divisions: 46,
-                //       onChanged: (double value) {
-                //         sliderUpdate(value);
-                //       }),
-                // ),
-                ElevatedButton(
-                  onPressed: () {
-                    reset();
-                  },
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(mine)),
-                  child: const Icon(
-                    Icons.restart_alt_outlined,
-                    color: Colors.white,
+        appBar: AppBar(
+          title: Text("${widget.courseCode} Quiz"),
+        ),
+        body: FutureBuilder(
+            future: getData(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return const Center(
+                  child: Text(
+                    'Could not load quiz. Make sure your internet is good and check back later.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 18),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        reset();
-                      },
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(mine)),
-                      child: Text(
-                        "${_currentSliderValue.toInt()} / ${filteredQuestions.length}",
-                        style: const TextStyle(color: Colors.white),
-                      )),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              // color: const Color.fromARGB(255, 244, 242, 238),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Text(
-              '${filteredQuestions[_questionIndex]["questionText"]}',
-              style: TextStyle(fontSize: 20, color: mine.shade900),
-            ),
-          ),
-          Column(
-            children: filteredQuestions[_questionIndex]["answers"]
-                .map<Widget>((answer) {
-              String label; //place holder for option label
-              if (labelcounter == 0) {
-                label = "A";
-              } else if (labelcounter == 1) {
-                label = "B";
-              } else if (labelcounter == 2) {
-                label = "C";
+                );
               } else {
-                label = 'D';
-              }
-              // increase the counter to change label
-              labelcounter++;
-              return Answer(
-                callBack: play,
-                optionValue: '$answer',
-                optionLable: label,
-                correctOption:
-                    '${filteredQuestions[_questionIndex]["correct"]}',
-              );
-            }).toList(),
-          ),
+                return ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        children: [
+                          // Expanded(
+                          //   child: Slider(
+                          //       activeColor: mine,
+                          //       thumbColor: mine.shade100,
+                          //       value: _currentSliderValue,
+                          //       max: filteredQuestions.length.toDouble(),
+                          //       // divisions: 46,
+                          //       onChanged: (double value) {
+                          //         sliderUpdate(value);
+                          //       }),
+                          // ),
+                          ElevatedButton(
+                            onPressed: () {
+                              reset();
+                            },
+                            style: ButtonStyle(
+                                backgroundColor: WidgetStateProperty.all(mine)),
+                            child: const Icon(
+                              Icons.restart_alt_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  reset();
+                                },
+                                style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all(mine)),
+                                child: Text(
+                                  "${_currentSliderValue.toInt()} / ${filteredQuestions.length}",
+                                  style: const TextStyle(color: Colors.white),
+                                )),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 20),
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        // color: const Color.fromARGB(255, 244, 242, 238),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        '${filteredQuestions[_questionIndex]["questionText"]}',
+                        style: TextStyle(fontSize: 20, color: mine.shade900),
+                      ),
+                    ),
+                    Column(
+                      children: filteredQuestions[_questionIndex]["answers"]
+                          .map<Widget>((answer) {
+                        String label; //place holder for option label
+                        if (labelcounter == 0) {
+                          label = "A";
+                        } else if (labelcounter == 1) {
+                          label = "B";
+                        } else if (labelcounter == 2) {
+                          label = "C";
+                        } else {
+                          label = 'D';
+                        }
+                        // increase the counter to change label
+                        labelcounter++;
+                        return Answer(
+                          callBack: play,
+                          optionValue: '$answer',
+                          optionLable: label,
+                          correctOption:
+                              '${filteredQuestions[_questionIndex]["correct"]}',
+                        );
+                      }).toList(),
+                    ),
 
-          // Use map to transform each answer into an Answer widget
-        ],
-      ),
-    );
+                    // Use map to transform each answer into an Answer widget
+                  ],
+                );
+              }
+            }));
   }
 }
 
