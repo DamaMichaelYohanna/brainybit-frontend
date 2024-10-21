6bdc8b77ab92e4e13b833f7648ea7a36a50cb53b
DamaMichaelYohanna <get2dama11@gmail.com>
2024-10-21 23:36:19 +0100

    random update

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:brainybit/colorScheme.dart';
 import 'package:brainybit/home_menu.dart';
 import 'package:brainybit/login.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 bool isPremium = false;
 String name = '';
+String email = '';
+Map<dynamic, dynamic> questions = {};
 Future<void> main() async {
   // Function to fetch user token to check if the user is logged in.
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  // RequestConfiguration requestConfiguration = RequestConfiguration(
+  //     testDeviceIds: ['bbdfd586-6bc3-465f-875c-10ca1ad1abe1']);
+  // MobileAds.instance.updateRequestConfiguration(requestConfiguration);
   Future<bool> getUserToken() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     name = prefs.getString("fullName") ?? "";
